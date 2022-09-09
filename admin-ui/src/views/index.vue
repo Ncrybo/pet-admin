33368341104cbae328c6fdf968d1f5d570381b8a
bobo <570105891@qq.com>
2022-09-09 14:00:32 +0800

    修改了主页面和标题，更换数库登录账号

--- admin-ui/src/views/index.vue
+++ admin-ui/src/views/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="app-container home">
-    haha
+    <div class="title">
+      <h1>欢迎使用后台管理系统</h1>
+    </div>
   </div>
 </template>
 
@@ -10,6 +12,19 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+#app {
+  .app-container {
+    width: 100%;
+    height: calc(100vh - 84px);
+    overflow: hidden;
+
+    .title {
+      width: 100%;
+      text-align: center;
+    }
+  }
+}
+</style>>
 
 </style>
